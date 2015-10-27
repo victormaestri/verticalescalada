785b7f53bc01daf4b95918fd883c94c1da6cee07
Victor Maestri <victormaestri.f@gmail.com>
2015-10-27 15:32:44 -0200

    Criando arquivo index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+	<title>Vertical Escalada</title>
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description"  content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="Victor Maestri" />
+	<meta name="revisit-after" content="7 days" />
+	
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="/css/padrao.css" />
+
+</head>
+
+<body>
+	<div id="Header"></div>
+	
+	<div id="Article"></div>
+	
+	<div id="Footer"></div>
+</body>
+</html>
